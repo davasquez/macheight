8078c07b80f5afc2767f38291ad864a5a47bd969
Daniel Vasquez <daniel.vasquez@fixiie.com>
2021-12-08 03:00:39 -0500

    Avoid duplicated pairs and pair with itself.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import requests
 import argparse
 
 def get_total_height():
+    """Parse and validate the integer input to the program and return it."""
+
     parser = argparse.ArgumentParser(description='Macheight Test')
 
     parser.add_argument('height', type=int,
@@ -16,61 +18,139 @@ def get_total_height():
     return args.height
 
 def get_players():
-    
+    """Get all players from the repo and return them in a list.
+
+    For simplicity, we assume that each player have all attributes
+    well-defined. Therefore, we don't validate them.
+    """
+
     players = requests.get('https://mach-eight.uc.r.appspot.com/')
-    
+
     if players.status_code != 200:
         sys.exit("Error reading players data")
-    
+
     players = players.json()
-    
+
     if not players['values']:
         sys.exit("Players data not defined")
-    
+
     return players['values']
 
 def get_name(p):
+    """Return the player name together with the height for debugging purpose."""
+
     return p['first_name'] + ' ' + p['last_name'] + '(' + p['h_in'] + ')'
 
-def get_group_name(p):
-    return '    - ' + get_name(p) + '\n'
+def get_players_names(players):
+    """Return a dictionary which maps indexes with players names.
 
-def get_main_name(p):
-    return get_name(p) + ':\n'
+    Complexity: O(n)
+    """
+
+    names = {}
+    idx = 0
+    for p in players:
+        names[str(idx)] = get_name(p)
+        idx += 1
+
+    return names
 
 def get_players_group_by_height(players):
+    """Group all players based on the height in inches.
+
+    Return:
+    Dictionary where each key corresponds to players height and
+    the values correpond to a concatenated string of indexes, where
+    each index corresponds to a particular player.
+
+    Complexity: O(n)
+    """
 
     players_group_by_height = {}
-
+    idx = 0
     for p in players:
         if not p['h_in'] in players_group_by_height:
-            players_group_by_height[p['h_in']] = get_group_name(p)
+            players_group_by_height[p['h_in']] = '-' + str(idx) + ' '
         else:
-            players_group_by_height[p['h_in']] += get_group_name(p)
+            players_group_by_height[p['h_in']] += '-' + str(idx) + ' '
+        idx += 1
 
     return players_group_by_height
 
-def print_players_pairs(height_total, players, players_group_by_height):
+def print_players_pairs(total_height, players, players_group_by_height, names):
+    """Print a list of all pairs of players whose height adds up to total_height
 
-    players_pairs = ''
-    height_used = {}
+    Keyword arguments:
+    total_height -- integer value
 
+    Complexity: O(n)
+    """
+
+    # The following block has a complexity of O(n) where n is the number of
+    # players
+    players_pairs_tmp = ''
+    height_used = {} # avoid duplicated pairs
+    idx = 0
     for p in players:
-        height = str(height_total - int(p['h_in']))
+        height = str(total_height - int(p['h_in']))
         if not p['h_in'] in height_used and height in players_group_by_height:
-            players_pairs += get_main_name(p) + players_group_by_height[height]
+            players_pairs_tmp += str(idx) + ' ' + players_group_by_height[height]
             height_used[height] = True
+        idx += 1
 
-    print(players_pairs)
+    # The split built-in function has a complexity of O(m) where m is the number
+    # of characters in the string. For the current case, m can be represented as
+    # k*n, where k is (to some extend) the number of digits of each index
+    # representing each player, and n in the number of players. For the case of
+    # the Big-O notation, the constant k can be left out, leading to a
+    # complexity of O(n).
+    players_pairs_tmp = players_pairs_tmp.split()
 
-def main():
+    # The following block has a complexity of O(n) where n is roughly the number
+    # of players
+    players_pairs = ''
+    for p in players_pairs_tmp:
+        if p[0] != '-':
+            p1 = p
+            continue
+        # The following if-statement avoids pairing a player with himself
+        if '-' + p1 != p:
+            players_pairs += names[p1] + ' - ' + names[p[1:]] + '\n'
 
-    total_height = get_total_height()
+    if players_pairs == '':
+        print('No matches found')
+    else:
+        print(players_pairs)
+
+def main(total_height):
+    """Print a list of all pairs of players whose height adds up to total_height
+
+    Keyword arguments:
+    total_height -- integer value
+
+    Edge cases considered:
+    - Avoid duplicated pairs of players.
+    - Avoid pairing a player with himself.
+
+    Complexity:
+    Each block of code has a complexity of n. If we join the complexity of all
+    blocks, it would lead to n + n + n + ... = kn, with k << n. Therefore,
+    k can be left out, leading to a complexity of O(n).
+    """
 
     players = get_players()
 
+    # complexity O(n)
+    names = get_players_names(players)
+
+    # complexity O(n)
     players_group_by_height = get_players_group_by_height(players)
 
-    print_players_pairs(total_height, players, players_group_by_height)
+    # complexity O(n)
+    print_players_pairs(total_height, players, players_group_by_height, names)
 
-main()
+if __name__ == '__main__':
+    """Command line call."""
+
+    total_height = get_total_height()
+    main(total_height)
